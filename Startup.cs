c75ababfb0f602d136cdfa3d44d9aae98fead375
XTheTap <illia9842@gmail.com>
2021-09-17 08:41:46 +0300

    Refresh commit

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,6 @@ namespace AutoServiceGeniralsMotors
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            //Configuration["DbServer"]
             services.AddDatabaseDeveloperPageExceptionFilter();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
