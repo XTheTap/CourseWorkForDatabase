00d55e79669130394e8283f53966ff9d33befa58
XTheTap <illia9842@gmail.com>
2021-10-09 12:43:39 +0300

    Final

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -17,9 +17,9 @@ namespace AutoServiceGeniralsMotors.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return Page();
         }
 
         public IActionResult OnPostWrite()
